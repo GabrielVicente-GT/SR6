09431f29bd58b149fe6463ef43fbeca3730524ac
Gabriel <chentevilo2001@gmail.com>
2022-11-19 21:47:20 -0600

    SR6

--- vector.py
+++ vector.py
@@ -1,13 +1,10 @@
-from numpy import *
+from math import *
 class V3(object):
     def __init__(self,x,y = 0,z = 0,w=1):
-        if (type(x) == matrix):
-            self.x, self.y, self.z, self.w = x.tolist()[0]
-        else:
-            self.x = x
-            self.y = y
-            self.z = z
-            self.w = w
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
 
     def round(self):
         self.x = round(self.x)
@@ -44,16 +41,13 @@ class V3(object):
         return self.x*other.x + self.y*other.y + self.z*other.z
 
     def len(self):
-        return(self.x**2 + self.y**2 + self.z**2)**0.5
+        return(self.z**2 +self.y**2 + self.x**2)**(1/2)
 
     def normalize(self):
-        try: #Si el vector no es cero, entonces normaliza.
+        try:
             return self * (1 / self.len())
-        except: #Si el vector es cero, entonces devuelve un vector cero.
+        except:
             return V3(-1, -1, -1)
-
-    def __repr__(self):
-        return "V3(%s,%s,%s)" & (self.x,self.y,self.z)
 
     def __str__(self):
         cadena = str(self.x)+" "+str(self.y)+" "+str(self.z)+" "
